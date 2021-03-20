3e82c33eb4faa9c6647e0f39f739c829e9577a5c
Håkon Bråten <haakbraa@gmail.com>
2021-03-20 16:28:12 +0100

    Improced performance

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,17 @@
-Selectize.define('hidden_textfield', function(options) {
+Selectize.define('hidden_textfield', function (options) {
     var self = this;
-    this.showInput = function() {
-         this.$control.css({cursor: 'pointer'});
-         this.$control_input.css({opacity: 0, position: 'relative', left: self.rtl ? 10000 : -10000 });
-         this.isInputHidden = false;
-     };
+    this.showInput = function () {
+        this.$control.css({ cursor: 'pointer' });
+        this.$control_input.css({ opacity: 0, position: 'relative', left: self.rtl ? 10000 : -10000 });
+        this.isInputHidden = false;
+    };
 
-     this.setup_original = this.setup;
+    this.setup_original = this.setup;
 
-     this.setup = function() {
-          self.setup_original();
-          this.$control_input.prop("disabled","disabled");
-     }
+    this.setup = function () {
+        self.setup_original();
+        this.$control_input.prop("disabled", "disabled");
+    }
 });
 
 var apiGetUsers = "https://turotrackerfetcher.azurewebsites.net/api/users?code=W0cY9erJuRhGLxSra7jDufItw6SRNnkPaHCw7pzmnLrRO9xa2Uln2w==";
@@ -20,69 +20,54 @@ var apiGetStats = "https://turotrackerfetcher.azurewebsites.net/api/stats?code=C
 
 var selectedUserIDs = [];
 var selectedClub = 0;
+var selectedMode = 1;
 
 var users = {};
+var usersLoaded = false;
+
 var clubs = {};
 
 var data = {};
-var mode = 1;
-
-var usersLoaded = false;
-var clubsLoaded = false;
+var statsData = null;
 
 var graph = null;
 
-var urlParams = new URLSearchParams(window.location.search);
-
-function onLoad()
-{
-    if (!usersLoaded || !clubsLoaded)
-    {
-        return;
+function UpdateUrlParameters() {
+    var urlParams = new URLSearchParams();
+    if (selectedClub != 0) {
+        urlParams.set('club', selectedClub)
     }
 
-    console.log("done loading")
-    parseUrl();
-}
+    if (selectedUserIDs.length > 0) {
+        urlParams.set('users', selectedUserIDs.join(','))
+    }
 
-function urlParamsUpdated()
-{
+    if (selectedMode != 1) {
+        urlParams.set('mode', selectedMode)
+    }
+
     var loc = window.location;
     var url = `${loc.origin}${loc.pathname}?${urlParams.toString()}`
-    console.log(url)
     history.replaceState(null, '', url);
 }
 
-function parseUrl()
-{
-    console.log("parsing url")
-
-    if (urlParams.has('mode'))
-    {
-        console.log("Found mode " + mode)
-        selectMode(urlParams.get('mode'))
+function parseUrl() {
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('mode')) {
+        SetMode(urlParams.get('mode'))
     }
 
-    if (urlParams.has('club'))
-    {
-        console.log("Found club " + selectedClub)
-        selectClub(urlParams.get('club'))
+    if (urlParams.has('club')) {
+        SetClub(urlParams.get('club'))
     }
-    
-    if (urlParams.has('users'))
-    {
+
+    if (urlParams.has('users')) {
         var userIDs = urlParams.get('users').split(',')
-        console.log("Found users " + userIDs);
-        userIDs.forEach( (userID, index) => selectUser(userID));
+        userIDs.forEach((userID, index) => AddUser(userID));
     }
 }
 
-function delay(time, func)
-{
-    setTimeout(func, time)
-}
-
-$(document).ready( function() {
+$(document).ready(function () {
     $('#selectMode').selectize({
         plugins: ['hidden_textfield']
     });
@@ -90,210 +75,169 @@ $(document).ready( function() {
         plugins: ['hidden_textfield']
     });
     $('#selectUser').selectize({
-        onDropdownOpen: function(dropdown) {
-                this.clear(true)
+        onDropdownOpen: function (dropdown) {
+            this.clear(true)
         }
     });
 
     var select = $('#selectUser')[0].selectize
     select.setTextboxValue("Laster deltakere...")
-    
+
     var select = $('#selectClub')[0].selectize
     select.setTextboxValue("Laster arrangører...")
 
-    $.get(apiGetUsers, function ( data ) {
+    $.get(apiGetUsers, function (data) {
+        console.log("Users returned")
         var select = $('#selectUser')[0].selectize
         select.clearOptions();
-        select.addOption({value: -1, text:"Legg til deltaker"})
-        select.addOption({value: -1, text:"------------"})
-        $.each(data, function(i, user) {
+        select.addOption({ value: -1, text: "Legg til deltaker" })
+        select.addOption({ value: -1, text: "------------" })
+        $.each(data, function (i, user) {
             users[user.userID] = user;
-            select.addOption({value:user.userID, text:user.name})
+            select.addOption({ value: user.userID, text: user.name })
         });
         select.setValue(-1, true);
 
         usersLoaded = true;
-        onLoad();
-        
-        $.get(apiGetStats, function ( data ) {
-            console.log(data)
-            var userStrs = data.map(stats => users[stats.userID].name + " [" + stats.score + "]");
-            var userIds = data.map(stats => stats.userID)
-            var link = "index.html?users=" + userIds.join(",") + "&mode=2"
-            var str = "<a href=\"" + link + "\">Flest klipp siste 5 dager</a>: " + userStrs.join(", ")
-            $('#footer').html(str)
-        });
+        UpdateStats();
+        RedrawGraph();
     });
-    
-    $.get(apiGetClubs, function ( data ) {
-        clubs[0] = {clubID: 0, name: "Norge"}
+
+    $.get(apiGetClubs, function (data) {
+        console.log("Clubs returned")
+        clubs[0] = { clubID: 0, name: "Norge" }
         var select = $('#selectClub')[0].selectize
         select.clearOptions();
-        select.addOption({value: 0, text:"Norge"})
-        select.addOption({value: -1, text:"------------"})
-        $.each(data, function(i, club) {
+        select.addOption({ value: 0, text: "Norge" })
+        select.addOption({ value: -1, text: "------------" })
+        $.each(data, function (i, club) {
             clubs[club.clubID] = club;
-            select.addOption({value:club.clubID, text:club.name})
+            select.addOption({ value: club.clubID, text: club.name })
         });
-        select.setValue(0, true);
-
-        clubsLoaded = true;
-        onLoad();
+        select.setValue(selectedClub, true);
     });
+
+    $.get(apiGetStats, function (data) {
+        console.log("Stats returned")
+        statsData = data;
+        UpdateStats();
+    });
+
+    CreateGraph();
+    parseUrl();
 });
+
+function UpdateStats() {
+    if (statsData == null || !usersLoaded) {
+        return;
+    }
+
+    var userStrs = statsData.map(stats => users[stats.userID].name + " [" + stats.score + "]");
+    var userIds = statsData.map(stats => stats.userID)
+    var link = "index.html?users=" + userIds.join(",") + "&mode=2"
+    var str = "<a href=\"" + link + "\">Flest klipp siste 5 dager</a>: " + userStrs.join(", ")
+    $('#footer').html(str)
+}
 
 function clubSelected(selector) {
     var clubID = parseInt(selector.options[selector.selectedIndex].value);
-    if  (clubID  == -1 || isNaN(clubID))
-    {
-        console.log("Selected 0")
+    if (clubID == -1 || isNaN(clubID)) {
         $('#selectClub')[0].selectize.setValue(0, false)
         return;
     }
 
-    selectClub(clubID);
-    urlParams.set("club", clubID);
-    urlParamsUpdated();
+    SetClub(clubID);
 }
 
-function selectClub(clubID) {
-    if (clubID == selectedClub || isNaN(clubID))
-    {
+function SetClub(clubID) {
+    if (clubID == selectedClub || isNaN(clubID)) {
         return;
     }
 
     selectedClub = clubID;
     $('#selectClub')[0].selectize.setValue(clubID, false)
 
+    ClearGraph();
+    UpdateUrlParameters();
+
     userData = {}
-    selectedUserIDs.forEach( (userID, index) => getDataForUser(userID));
+    selectedUserIDs.forEach((userID, index) => GetDataForUser(userID));
 }
 
 
 function userSelected(selector) {
     var userID = parseInt(selector.options[selector.selectedIndex].value);
-    if (userID  == -1)
-    {
+    if (userID == -1) {
         return;
     }
-    
+
     $('#selectUser')[0].selectize.setValue(-1, false);
-    selectUser(userID);
+    AddUser(userID);
 }
 
-function selectUser(userID)
-{
+function AddUser(userID) {
     var index = selectedUserIDs.indexOf(userID);
-    if (index != -1 || isNaN(userID))
-    {
+    if (index != -1 || isNaN(userID)) {
         return;
     }
 
     selectedUserIDs.push(userID);
 
-    getDataForUser(userID);
-    urlParams.set("users", selectedUserIDs.join(','));
-    urlParamsUpdated();
+    GetDataForUser(userID);
+    UpdateUrlParameters();
 }
 
-function selectMode(newMode)
-{
-    if (newMode == mode || isNaN(newMode))
-    {
+function SetMode(mode) {
+    if (mode == selectedMode || isNaN(mode)) {
         return;
     }
 
-    mode = newMode;
-    $('#selectMode')[0].selectize.setValue(newMode, false);
-    dataUpdated();
-    urlParams.set("mode", mode);
-    urlParamsUpdated();
+    selectedMode = mode;
+    $('#selectMode')[0].selectize.setValue(mode, false);
+
+    RedrawGraph();
+    UpdateUrlParameters();
 }
 
 function modeSelected(selector) {
     var newMode = parseInt(selector.options[selector.selectedIndex].value);
-    selectMode(newMode)
+    SetMode(newMode)
 }
 
-function getDataForUser(userID)
-{
-    if  (selectedClub == null)
-    {
+function GetDataForUser(userID) {
+    if (selectedClub == null) {
         return;
     }
 
     console.log(`Getting data for user [${userID}] in club [${selectedClub}]`)
     var url = `https://turotrackerfetcher.azurewebsites.net/api/scores/${selectedClub}/${userID}?code=D2DC7NmU0RuGdoKvH6VFfDAaPDkM5Zy9SxXyNZkOoP8uQfoJ8/6wKA==`
 
-    $.get(url, function ( userData ) {
+    $.get(url, function (userData) {
         data[userID] = userData;
-        dataUpdated();
+        console.log("Got data for " + userID)
+        AddUserGraph(userID);
     });
 }
 
-function userRemoved(userID)
-{
+function RemoveUser(userID) {
     var index = selectedUserIDs.indexOf(userID);
-    if (index == -1)
-    {
+    if (index == -1) {
         return
     }
 
     selectedUserIDs.splice(index, 1);
     delete data[userID];
-    dataUpdated();
 
-    urlParams.set("users", selectedUserIDs.join(','));
-    urlParamsUpdated();
+    RemoveUserGraph(userID)
+    UpdateUrlParameters();
 }
 
-function legendHandler(e, legendItem)
-{
-    console.log(legendItem);
-    var userName = legendItem.text;
-    for (key in selectedUserIDs)
-    {
-        var userID = selectedUserIDs[key]
-        if (users[userID].name == userName)
-        {
-            userRemoved(userID);
-            return
-        }
-    }
-}
-
-function dataUpdated()
-{
-    var datasets = [];
-    for (var key in data)
-    {
-        var userData = data[key];
-        var userValues;
-        if (mode == 4) { userValues = userData.map(ud => {return {x: ud.date, y: ud.rankPunches, label: ud.rankPunches + "."}} ) }
-        else if (mode == 3) { userValues = userData.map(ud => {return {x: ud.date, y: ud.rankPoints, label: ud.rankPoints + "."}}) }
-        else if (mode == 2) { userValues = userData.map(ud => {return {x: ud.date, y: ud.punches, label: ud.punches}}) }
-        else { userValues = userData.map(ud => {return {x: moment(ud.date), y: ud.points, label: ud.points}}) }
-
-        var dataset = {
-                label: users[key].name,
-                data : userValues,
-                fill: false
-        };
-
-        console.log(dataset);
-        datasets.push(dataset);
-    }
-
+function CreateGraph() {
     var ctx = document.getElementById('graph').getContext('2d');
-    if (graph != null)
-    {
-        graph.destroy();
-    }
-
     graph = new Chart(ctx, {
         type: 'line',
         data: {
-            datasets: datasets
+            datasets: []
         },
         options: {
             layout: {
@@ -327,9 +271,8 @@ function dataUpdated()
                     scheme: 'brewer.SetTwo8'
                 },
                 datalabels: {
-                    display: function(context) {
-                        if (context.dataset.data.length == (context.dataIndex + 1))
-                        {
+                    display: function (context) {
+                        if (context.dataset.data.length == (context.dataIndex + 1)) {
                             return true
                         }
 
@@ -348,4 +291,73 @@ function dataUpdated()
             }
         }
     });
+}
+
+function legendHandler(e, legendItem) {
+    var userName = legendItem.text;
+    for (key in selectedUserIDs) {
+        var userID = selectedUserIDs[key]
+        if (users[userID].name == userName) {
+            RemoveUser(userID);
+            return
+        }
+    }
+}
+
+function ClearGraph(update = true) {
+    graph.data.datasets = []
+
+    if (update) {
+        graph.update()
+    }
+}
+
+function RedrawGraph() {
+    ClearGraph();
+
+    for (userID in data) {
+        AddUserGraph(userID, false)
+    }
+
+    graph.update();
+}
+
+function RemoveUserGraph(userID, update = true) {
+    var index = graph.data.datasets.findIndex(ds => ds.userID == userID)
+    graph.data.datasets.splice(index, 1)
+
+    if (update) {
+        graph.update();
+    }
+}
+
+function AddUserGraph(userID, update = true) {
+    if (!usersLoaded) {
+        console.log("Users not loaded yet, returning")
+        return;
+    }
+
+    if (graph.data.datasets.some(ds => ds.userID == userID)) {
+        console.log("User " + userID + " already added");
+        return;
+    }
+
+    if (selectedMode == 4) { userValues = data[userID].map(ud => { return { x: moment(ud.date, "YYYY-MM-DD"), y: ud.rankPunches, label: ud.rankPunches + "." } }) }
+    else if (selectedMode == 3) { userValues = data[userID].map(ud => { return { x: moment(ud.date, "YYYY-MM-DD"), y: ud.rankPoints, label: ud.rankPoints + "." } }) }
+    else if (selectedMode == 2) { userValues = data[userID].map(ud => { return { x: moment(ud.date, "YYYY-MM-DD"), y: ud.punches, label: ud.punches } }) }
+    else { userValues = data[userID].map(ud => { return { x: moment(ud.date, "YYYY-MM-DD"), y: ud.points, label: ud.points } }) }
+
+    var dataset = {
+        label: users[userID].name,
+        data: userValues,
+        fill: false,
+        userID: userID
+    };
+
+    console.log("Added data for user " + userID)
+    graph.data.datasets.push(dataset);
+
+    if (update) {
+        graph.update();
+    }
 }
